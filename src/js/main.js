693a9c93ae2d5dc93d4094f37bd860e9b7d9e076
Yaroslav <yRogovich@gmail.com>
2019-07-15 13:31:41 +0300

    Updated toggle animaton of mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -4,11 +4,7 @@
      */
     $(function() {
         $('.ham').click(function() {
-            $('.navbar-bottom').toggleClass('active');
-            var btn_width = btn.width();
-            btn.width(0);
-            $('.navbar-bottom').animate({width: btn_width }, 500);
-
+            $('.navbar-bottom').fadeToggle();
         });
         
     });
